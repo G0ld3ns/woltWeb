54669e8b460798848fa8e63bdfbe4627356b630a
Karolis <karolisa234@gmail.com>
2025-12-07 17:43:35 +0200

    webver1.1.

--- src/main/java/com/example/woltweb/controllers/UserController.java
+++ src/main/java/com/example/woltweb/controllers/UserController.java
@@ -5,26 +5,68 @@ import com.example.woltweb.repo.BasicUserRepo;
 import com.example.woltweb.repo.DriverRepo;
 import com.example.woltweb.repo.RestaurantRepo;
 import com.example.woltweb.repo.UserRepo;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Properties;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepo userRepo;
-    private DriverRepo driverRepo;
-    private BasicUserRepo basicUserRepo;
-    private RestaurantRepo restaurantRepo;
 
-    @GetMapping(value =  "getAllUsers")
-
+    @GetMapping(value =  "getAllUsers") // tai ka kvieciame http (http://localhost:8080/getAllUsers <---- sitas
     public @ResponseBody Iterable<User> getAllUsers(){
         return userRepo.findAll();
     }
 
 
+    @GetMapping(value = "validateUser")
+    public @ResponseBody User getUserByCredentials(@RequestBody String info){
+        System.out.println(info);
+        Gson gson = new Gson();
+        Properties properties = gson.fromJson(info, Properties.class);
+        var login = properties.getProperty("username");
+        var psw = properties.getProperty("password");
+        return userRepo.getUserByLogin(login, psw);
+    }
+
+    @PutMapping(value = "updateUser")
+    public @ResponseBody User updateUser(@RequestBody User user){
+        userRepo.save(user);
+        return userRepo.getReferenceById(user.getId());
+    }
+
+    @PutMapping(value = "updateUserById/{id}")
+    public @ResponseBody User updateUserById(@RequestBody String info, @PathVariable int id){
+        User user = userRepo.findById(id).orElseThrow(()-> new RuntimeException("user not found"));
+        Gson gson = new Gson();
+        Properties properties = gson.fromJson(info, Properties.class);
+        var name = properties.getProperty("name");
+        user.setName(name);
+        userRepo.save(user);
+        return userRepo.getReferenceById(user.getId());
+    }
+
+    @PostMapping(value = "insertUser")
+    public @ResponseBody User createUser(@RequestBody User user){
+        userRepo.save(user);
+        return userRepo.getUserByLogin(user.getLogin(), user.getPassword());
+
+    }
+
+    @DeleteMapping(value = "deleteUser")
+    public @ResponseBody String deleteUser(@PathVariable int id){
+        userRepo.deleteById(id);
+        User user = userRepo.findById(id).orElse(null);
+        if (user !=null) {
+            return "Failed to delete user";
+        } else {
+            return "Successfully deleted user";
+        }
+
+    }
 
 }
